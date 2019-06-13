8ef6efd184cffd277cac9383ca8fec692e753e24
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 10:58:57 +0300

    Validations

---
+++ SiteNewsApi/Models/Validations/NewsValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SiteNewsApi.Models.Entities;
+
+namespace SiteNewsApi.Models.Validations
+{
+    public class NewsValidator : AbstractValidator<News>
+    {
+        public NewsValidator()
+        {
+            RuleFor(u => u.UsersNews)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
---
+++ SiteNewsApi/Models/Validations/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SiteNewsApi.Models.DTOs;
+
+namespace SiteNewsApi.Models.Validations
+{
+    public class UserValidator : AbstractValidator<UserDTO>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Email)
+                .Null()
+                .Empty()
+                .EmailAddress();
+            RuleFor(u => u.Password)
+                .NotNull()
+                .NotEmpty()
+                .Length(6, 25)
+                .WithMessage("Password can not be empty");
+            RuleFor(u => u.Login)
+                .NotNull()
+                .NotEmpty()
+                .Length(6, 50)
+                .WithMessage("Login can not be empty");
+        }
+    }
+}
