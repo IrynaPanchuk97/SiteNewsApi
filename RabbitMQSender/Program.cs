319ba517bbe0f1c8a30c61ce30c3a5f31508393e
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 11:58:29 +0300

    RabbitMQSender

---
+++ RabbitMQSender/Program.cs
@@ -0,0 +1,21 @@
+﻿using SeleniumWebDriver;
+using System;
+using System.Threading;
+
+namespace RabbitMQSender
+{
+    class Program
+    {
+        static void Main()
+        {
+            var timer = new Timer(Timer, null, 0, 20000);
+            Console.ReadLine();
+        }
+        static void Timer(object o)
+        {
+            var dataReader = new GetNews();
+            var news = dataReader.Crawl();
+            news.ForEach(Sender.Send);
+        }
+    }
+}
---
+++ RabbitMQSender/Sender.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using SiteNewsApi.Models.Entities;
+using System.Text;
+
+namespace RabbitMQSender
+{
+    public class Sender
+    {
+        public static void Send(News news)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(news));
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "hello",
+                    basicProperties: properties,
+                    body: body);
+            }
+        }
+    }
+}
