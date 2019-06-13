76d473618deead63a3d76baa130eb4ee800b95e4
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 11:00:34 +0300

    Context

---
+++ SiteNewsApi/Models/NewsContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteNewsApi.Models.Entities;
+
+namespace SiteNewsApi.Models
+{
+    public class NewsContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UsersNews> UsersNews { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UsersNews>()
+                .HasKey(a => new { a.IdUser, a.IdNews });
+
+            modelBuilder.Entity<UsersNews>()
+                .HasOne(a => a.News)
+                .WithMany(b => b.UsersNews)
+                .HasForeignKey(a => a.IdNews);
+
+            modelBuilder.Entity<UsersNews>()
+                .HasOne(a => a.User)
+                .WithMany(b => b.UsersNews)
+                .HasForeignKey(a => a.IdUser);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(local)\\SQLEXPRESS;Database=NewsDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        }
+    }
+}
