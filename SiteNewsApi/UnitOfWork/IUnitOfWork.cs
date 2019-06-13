18cf9228377159449013e7e2bcc7266f8945cb00
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-06-13 11:31:36 +0300

    UnitOfWork

---
+++ SiteNewsApi/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using SiteNewsApi.Repositories.InterfaceRepository;
+using System.Threading.Tasks;
+
+namespace SiteNewsApi.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        INewsRepository NewsRepository { get; }
+        IUserRepository UserRepository { get; }
+        Task<int> SaveAsync();
+    }
+}
---
+++ SiteNewsApi/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteNewsApi.Repositories.InterfaceRepository;
+using System.Threading.Tasks;
+
+namespace SiteNewsApi.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public INewsRepository NewsRepository { get; }
+
+        public IUserRepository UserRepository { get; }
+
+        public UnitOfWork(
+            DbContext context,
+            INewsRepository newsRepository,
+            IUserRepository userRepository)
+        {
+            _context = context;
+            NewsRepository = NewsRepository;
+            UserRepository = UserRepository;
+        }
+        public Task<int> SaveAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
